4b692ed13207094cb6169b99339806c7adb90ac4
wangwu <834667820@qq.com>
2018-01-08 14:23:07 +0800

    myTestProject

---
+++ src/main/java/Quartz/HelloQuartz.java
@@ -0,0 +1,17 @@
+package Quartz;
+
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.Date;
+
+public class HelloQuartz implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDetail detail = context.getJobDetail();
+        String name = detail.getJobDataMap().getString("name");
+        System.out.println("say hello to " + name + " at " + new Date());
+    }
+}
