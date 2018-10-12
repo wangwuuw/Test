6cea9f2575b40a9c336d5a5db8ceb65574fd41c2
wangwu <wangwu123mtr@sina.com>
2018-10-12 14:01:57 +0800

    测试版本号

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,6 @@ public class Test implements Runnable{
 
     public static void main(String[] args) {
         Test test = new Test();
-        System.out.println("版本4。0");
+        System.out.println("版本5。0");
     }
 }
